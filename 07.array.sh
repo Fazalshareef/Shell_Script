86ced8c3dff7d2663751e139923cd0bb7dea4d09
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-03-06 16:48:40 +0530

    added sixth script

--- 07.array.sh
+++ 07.array.sh
@@ -2,8 +2,8 @@
 
 DEVOPS=("Linux" "AWS" "Ansible" "Jenkins" "Terraform" "Docker" "Kubernetes")
 
-echo "First thing need to learn ${DEVOPS[0]}"
+echo "First thing need to learn is: ${DEVOPS[0]}"
 
-echo "Second thing need to learn ${DEVOPS[1]}"
+echo "Second thing need to learn is: ${DEVOPS[1]}"
 
-echo "Fifth thing need to learn ${DEVOPS[4]}"
+echo "Fifth thing need to learn is: ${DEVOPS[4]}"
