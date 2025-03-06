52ef20030513fea6b5c5a3b9b37d69eb8c8f21d5
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-03-06 15:32:19 +0530

    added second script

---
+++ 02.variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Mathew:: Hello John, Good Morning"
+echo "John:: Hello Mathew, Good Morning, How are you?"
+echo "Mathew:: Yeah good John, and what about ypu?"
+echo "John::yeah good" 
