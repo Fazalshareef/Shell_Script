e7c8727b5e7af31b93b016432f20b616e3c9c4c2
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-03-06 15:54:29 +0530

    added third script

---
+++ 04.aruguments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, Good Morning"
+echo "$PERSON2:: Hello $PERSON1, Good Morning, How are you?"
+echo "$PERSON1:: Yeah good $PERSON2, and what about ypu?"
+echo "$PERSON2:: yeah good $PERSON1" 
