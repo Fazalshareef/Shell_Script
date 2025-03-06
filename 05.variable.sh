45b3fc60985b315b9c1fc10fd8fb08552a1bdc92
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-03-06 16:07:37 +0530

    added fifth  script

---
+++ 05.variable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please enter your username :: "
+
+read -s USERNAME
+
+echo "Please enter your password : : "
+
+read -s PASSWORD
+
+echo "your username is $USERNAME and your password is $PASSWORD"
+
