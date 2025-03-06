37e9cfcf8f4c2fcd05151b10e4b4266f22715ff2
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-03-06 15:42:55 +0530

    added second script update

--- 02.variables.sh
+++ 02.variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Mathew:: Hello John, Good Morning"
-echo "John:: Hello Mathew, Good Morning, How are you?"
-echo "Mathew:: Yeah good John, and what about ypu?"
-echo "John::yeah good" 
+PERSON1=Mathew
+PERSON2=John
+
+echo "$PERSON1:: Hello $PERSON2, Good Morning"
+echo "$PERSON2:: Hello $PERSON1, Good Morning, How are you?"
+echo "$PERSON1:: Yeah good $PERSON2, and what about ypu?"
+echo "$PERSON2:: yeah good $PERSON1" 
