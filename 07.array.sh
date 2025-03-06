a865dc8b98e0c3e512f6b64797854bbec6a4978e
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-03-06 16:51:57 +0530

    added sixth script

--- 07.array.sh
+++ 07.array.sh
@@ -7,3 +7,5 @@ echo "First thing need to learn is: ${DEVOPS[0]}"
 echo "Second thing need to learn is: ${DEVOPS[1]}"
 
 echo "Fifth thing need to learn is: ${DEVOPS[4]}"
+
+echo "things to learn while preparing the devops role: ${DEVOPS[@]}"
