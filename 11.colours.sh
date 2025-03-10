5a8b8f18f91ce691ceed5dd3ac79b0084c87f8f9
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-03-10 12:22:58 +0530

    tenth script updated

---
+++ 11.colours.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Get user ID
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/${0##*/}-$TIMESTAMP.log"  # Store logs in /tmp
+
+# Define color codes
+RED="\e[31m"
+GREEN="\e[32m"
+RESET="\e[0m"
+
+# Validation function with color output
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "${RED}ERROR :: $2 failed${RESET}"
+        exit 1
+    else
+        echo -e "${GREEN}SUCCESS :: $2${RESET}"
+    fi
+}
+
+# Check if the user is root
+if [ "$ID" -ne 0 ]; then
+    echo -e "${RED}User is not the root user. Please provide root access.${RESET}"
+    exit 1
+else
+    echo -e "${GREEN}User has root access.${RESET}"
+fi
+
+# Install MySQL Server
+apt update >> $LOGFILE 2>&1
+apt install -y mysql-server >> $LOGFILE 2>&1
+VALID
