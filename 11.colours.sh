9f6005ccc4ffcbf1869166c9865fdb3e8ba1852a
Fazal Shareef <fazalshareef@Fazals-MacBook-Air.local>
2025-03-10 12:24:33 +0530

    tenth script updated

--- 11.colours.sh
+++ 11.colours.sh
@@ -30,7 +30,7 @@ fi
 
 # Install MySQL Server
 apt update >> $LOGFILE 2>&1
-apt install -y mysql-server >> $LOGFILE 2>&1
+apt install -y mysqlfwf-server >> $LOGFILE 2>&1
 VALIDATE $? "Installing MySQL"
 
 # Install Git
